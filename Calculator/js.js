28c3849424cf98facaa696d70739c80df60ff1be
Dheerajgrg777 <125276146+Dheerajgrg777@users.noreply.github.com>
2024-06-24 21:46:59 +0545

    Add files via upload

---
+++ Calculator/js.js
@@ -0,0 +1,15 @@
+let marks_of_class2=[56,35,6,54,true,"absenttt"];
+// console.log(marks_of_class2);
+// console.log(marks_of_class2[2]);
+// console.log(marks_of_class2[5]);
+// console.log("The length of marks is", marks_of_class2.length);
+// console.log(marks_of_class2[5].length);
+// //Array methond
+let b = marks_of_class2.toString();// b is now a string
+console.log(b);
+let c = marks_of_class2.join(" and ");//join 
+console.log(c);
+let d =marks_of_class2.pop();//pop the last element
+console.log(marks_of_class2);
+let e = marks_of_class2.push(300);//push the element
+console.log(marks_of_class2);
---
+++ Calculator/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+
+// let e = marks_of_class2.push(marks_of_class2[5]=300);
+// console.log(marks_of_class2);
